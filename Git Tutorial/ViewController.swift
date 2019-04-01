ba29a34b54d4fe16054109e4167a7bc80f1fbe1d
Kyle Koerber <kyle@lantiandesign.com>
2019-04-01 18:22:56 -0400

    Second commit

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(message)
         // Do any additional setup after loading the view.
     }
 
