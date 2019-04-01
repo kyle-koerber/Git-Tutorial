f09af2bc3312cdc6cbcafb8cd21bd06e4a7a56fb
Kyle Koerber <kyle@lantiandesign.com>
2019-04-01 18:19:17 -0400

    Initial commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Kyle Koerber on 4/1/19.
+//  Copyright © 2019 Kyle Koerber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let message = "Hello Git!"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(message)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
